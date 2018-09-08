6f9912acf3726ba2e66d07e5889870ff7160ea6c
Ivan Golikov <igolikov@octoberry.ru>
2018-09-08 14:43:38 +0300

    Now translation works

--- main.go
+++ main.go
@@ -7,7 +7,6 @@ import (
 	"io/ioutil"
 	"log"
 	"net/http"
-	"net/url"
 	"os"
 	"strings"
 	"time"
@@ -19,49 +18,65 @@ type TranslationResult struct {
 }
 
 func main() {
-	apiKey := os.Getenv("YANDEX-API-KEY")
+	apiKey := os.Getenv("YANDEX_API_KEY")
 	helloHandler := func(w http.ResponseWriter, r *http.Request) {
 
-		var reqValue string
-		for k, v := range r.Form {
-			if k == "tr" {
-				reqValue = strings.Join(v, "")
-			}
+		translationRequestValues, ok := r.URL.Query()["tr"]
+		if !ok || len(translationRequestValues[0]) < 1 {
+			log.Println("Invalid request")
 		}
-		request := url.QueryEscape(reqValue)
-		response := make_response(request, apiKey)
-		io.WriteString(w, response)
+		translationRequestValue := translationRequestValues[0]
+		response := getRequest(translationRequestValue, apiKey)
+		if _, err := io.WriteString(w, response); err != nil {
+			log.Println("Response output error")
+		}
 	}
 
 	http.HandleFunc("/", helloHandler)
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
 
-func make_response(w, apiKey string) string {
+func getRequest(w, apiKey string) string {
 	client := &http.Client{Timeout: 10 * time.Second}
 
 	urlString := fmt.Sprintf("https://translate.yandex.net/api/v1.5/tr.json/translate?key=%s&lang=en-ja&text=", apiKey)
 
 	url := urlString + w
 
-	resp, err := client.Get(url)
 	req, err := http.NewRequest("GET", url, nil)
-	req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
-	resp, err = client.Do(req)
+	if err != nil {
+		log.Println("Request initialization error")
+	}
 
-	defer resp.Body.Close()
+	req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
+	resp, err := client.Do(req)
+
+	if err != nil {
+		log.Println("Request execution error")
+	}
 
 	body, err := ioutil.ReadAll(resp.Body)
 
 	if err != nil {
-		fmt.Println("I/O Read Error")
+		log.Println("I/O Read Error")
 	}
+
 	var data TranslationResult
+
 	if err := json.Unmarshal(body, &data); err != nil {
-		fmt.Println("Unmarshalling error: ", err)
+		log.Println("Unmarshalling error: ", err)
 	}
+
+	defer resp.Body.Close()
+
 	if data.Code != 200 {
-		fmt.Printf("Error – code is %d", data.Code)
+		switch data.Code {
+		case 401:
+			log.Println("Invalid API Key")
+		default:
+			log.Printf("Error – code is %d", data.Code)
+		}
 	}
+
 	return strings.Join(data.Text, ",")
 }
