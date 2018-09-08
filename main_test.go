4fedf0b890cc1802dcd13cea335b5bb2480bc77c
Ivan Golikov <igolikov@octoberry.ru>
2018-09-08 18:44:20 +0300

    Added failure test case (no API Key)

--- main_test.go
+++ main_test.go
@@ -3,6 +3,7 @@ package main
 import (
 	"net/http"
 	"net/http/httptest"
+	"os"
 	"testing"
 )
 
@@ -26,5 +27,25 @@ func TestTranslationHandler(t *testing.T) {
 	if rr.Body.String() != expected {
 		t.Errorf("handler returned unexpected body: got %v want %v",
 			rr.Body.String(), expected)
+	}
+}
+
+func TestTranslationHandlerNoAPIKey(t *testing.T) {
+	if err := os.Unsetenv("YANDEX_API_KEY"); err != nil {
+		t.Errorf("Failed to unset env")
+	}
+	req, err := http.NewRequest("GET", "/?tr=man", nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	rr := httptest.NewRecorder()
+	handler := appHandler(TranslationHandler)
+
+	handler.ServeHTTP(rr, req)
+
+	if status := rr.Code; status == http.StatusOK {
+		t.Errorf("handler returned wrong status code: got %v instead of error",
+			status)
 	}
 }
