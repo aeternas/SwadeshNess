6580e98a450c1faa3e9bb5c9f2e8885e73b0cdb9
Ivan Golikov <igolikov@octoberry.ru>
2018-10-27 21:46:37 +0300

    Added credit parsing test

--- configuration/configuration_test.go
+++ configuration/configuration_test.go
@@ -12,11 +12,20 @@ func TestConfigurationRead(t *testing.T) {
 	}
 }
 
-func TestConfigurationParsing(t *testing.T) {
+func TestLanguagesParsing(t *testing.T) {
 	var reader *Reader = &Reader{Path: "db.json"}
 
 	config, _ := reader.ReadConfiguration()
 	if len(config.Languages) < 1 {
 		t.Errorf("No languages parsed from the DB")
+	}
+}
+
+func TestCreditsParsing(t *testing.T) {
+	var reader *Reader = &Reader{Path: "db.json"}
+
+	config, _ := reader.ReadConfiguration()
+	if len(config.Credits) < 1 {
+		t.Errorf("Invalid credits")
 	}
 }
