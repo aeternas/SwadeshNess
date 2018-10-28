0e7ff8a3c3af5aba37aa0293d70b85290f4b706e
Ivan Golikov <igolikov@octoberry.ru>
2018-10-28 17:28:49 +0300

    Refactored reader and configuration

--- configuration/configuration_test.go
+++ configuration/configuration_test.go
@@ -1,37 +1,20 @@
 package configuration
 
 import (
-	"errors"
 	Wrappers "github.com/aeternas/SwadeshNess/wrappers"
-	"os"
 	"testing"
 )
 
-var (
-	osWrapper   Wrappers.AnyOsWrapper
-	mockWrapper = new(MockOsWrapper)
-)
-
-func init() {
-}
-
 func TestConfigurationRead(t *testing.T) {
-	file, _ := os.Open("db.json")
-	mockWrapper.OpenStub = Wrappers.FileOpened{F: file, Err: errors.New("Err")}
-	osWrapper = mockWrapper
-	var reader *Reader = &Reader{Path: "db.json", OsWrapper: osWrapper}
+	var reader *Reader = &Reader{Path: "db.json", OsWrapper: new(Wrappers.OsWrapper)}
 	_, err := reader.ReadConfiguration()
-	if err != nil && mockWrapper.OpenWasCalled == 0 && mockWrapper.OpenArgs != "db.json" {
+	if err != nil {
 		t.Errorf("Failed to read configuration")
 	}
 }
 
 func TestLanguagesParsing(t *testing.T) {
-	file, _ := os.Open("db.json")
-	mockWrapper.OpenStub = Wrappers.FileOpened{F: file, Err: errors.New("Err")}
-	osWrapper = mockWrapper
-	var reader *Reader = &Reader{Path: "db.json", OsWrapper: osWrapper}
-
+	var reader *Reader = &Reader{Path: "db.json", OsWrapper: new(Wrappers.OsWrapper)}
 	config, _ := reader.ReadConfiguration()
 	if len(config.Languages) < 1 {
 		t.Errorf("No languages parsed from the DB")
@@ -39,35 +22,9 @@ func TestLanguagesParsing(t *testing.T) {
 }
 
 func TestCreditsParsing(t *testing.T) {
-	file, _ := os.Open("db.json")
-	mockWrapper.OpenStub = Wrappers.FileOpened{F: file, Err: errors.New("Err")}
-	osWrapper = mockWrapper
-	var reader *Reader = &Reader{Path: "db.json", OsWrapper: osWrapper}
-
+	var reader *Reader = &Reader{Path: "db.json", OsWrapper: new(Wrappers.OsWrapper)}
 	config, _ := reader.ReadConfiguration()
 	if len(config.Credits) < 1 {
 		t.Errorf("Invalid credits")
 	}
-}
-
-type MockOsWrapper struct {
-	GetEnvArgs      string
-	GetEnvWasCalled int
-	GetEnvStub      string
-
-	OpenArgs      string
-	OpenWasCalled int
-	OpenStub      Wrappers.FileOpened
-}
-
-func (w *MockOsWrapper) GetEnv(k string) string {
-	w.GetEnvWasCalled += 1
-	w.GetEnvArgs = k
-	return w.GetEnvStub
-}
-
-func (w *MockOsWrapper) Open(n string) Wrappers.FileOpened {
-	w.OpenWasCalled += 1
-	w.OpenArgs = n
-	return w.OpenStub
 }
---
+++ configuration/reader.go
@@ -0,0 +1,37 @@
+package configuration
+
+import (
+	"encoding/json"
+	"errors"
+	Wrappers "github.com/aeternas/SwadeshNess/wrappers"
+)
+
+type AnyReader interface {
+	ReadConfiguration() (Configuration, error)
+}
+
+type Reader struct {
+	Path      string
+	OsWrapper Wrappers.AnyOsWrapper
+}
+
+func (r *Reader) ReadConfiguration() (Configuration, error) {
+	lReader := *r
+	var p string = lReader.Path
+	fileOpened := lReader.OsWrapper.Open(p)
+	file := fileOpened.F
+	defer file.Close()
+	decoder := json.NewDecoder(file)
+	configuration := Configuration{}
+	err := decoder.Decode(&configuration)
+	if err != nil {
+		return Configuration{}, errors.New("Failed to read database")
+	}
+	apiKey := lReader.OsWrapper.GetEnv(API_KEY)
+	var translationEndpoint string = lReader.OsWrapper.GetEnv(TRANSLATION_ENDPOINT)
+	var groupEndpoint string = lReader.OsWrapper.GetEnv(GROUP_ENDPOINT)
+	configuration.ApiKey = apiKey
+	endpoints := Endpoints{TranslationEndpoint: translationEndpoint, GroupsEndpoint: groupEndpoint}
+	configuration.EEndpoints = endpoints
+	return configuration, nil
+}
