12895f8f9999068bc0c33be35a1218407bc4bd0a
Ivan Golikov <igolikov@octoberry.ru>
2018-11-02 15:45:15 +0300

    Commented out handler stub

--- handlers/GroupListHandler_test.go
+++ handlers/GroupListHandler_test.go
@@ -1,14 +1,17 @@
 package handlers
 
 import (
-	. "github.com/aeternas/SwadeshNess/language"
 	"testing"
 )
 
-func TestGroupListHandler(t *testing.T) {
+func TestGetGroupsGroupListHandler(t *testing.T) {
 	t.SkipNow()
-	gps := []LanguageGroup{LanguageGroup{Name: "Group:", Languages: []Language{{FullName: "Language", Code: "Lang"}}}}
+	/*gps := []LanguageGroup{LanguageGroup{Name: "Group:", Languages: []Language{{FullName: "Language", Code: "Lang"}}}}
+	var tConfig *Configuration = &Configuration{Languages: gps, ApiKey: "", Credits: "", EEndpoints: Endpoints{}}
+	gh := GroupListHandler{Config: tConfig}
+	var tResponseWriter *http.ResponseWriter = &http.ResponseWriter
+	gh.GetGroups(tResponseWriter, &http.Request)
 	if len(gps) > 0 {
 		t.Errorf("Err")
-	}
+	}*/
 }
