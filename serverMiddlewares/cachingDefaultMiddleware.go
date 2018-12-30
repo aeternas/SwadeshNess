830da0682178b051d933b0e655067e8828ab740b
Ivan Golikov <igolikov@octoberry.ru>
2018-12-30 14:18:39 +0300

    Now caching middleware tries to extract value

--- serverMiddlewares/cachingDefaultMiddleware.go
+++ serverMiddlewares/cachingDefaultMiddleware.go
@@ -21,11 +21,24 @@ func NewCachingDefaultServerMiddleware() CachingDefaultServerMiddleware {
 }
 
 func (c cachingDefaultServerMiddleware) AdaptRequest(r *apiClient.Request) *apiClient.Request {
-	log.Println(r)
+	key := getKey(r)
+	cw := c.CW
+	val, err := (*cw).GetCachedValue(key)
+	if err != nil {
+		log.Printf("Failed to extract cached value %s", key)
+		return r
+	}
+	bytes := []byte(val)
+	r.Cached = true
+	r.Data = bytes
 	return r
 }
 
 func (c cachingDefaultServerMiddleware) AdaptResponse(r *apiClient.Response) *apiClient.Response {
 	log.Println(r)
 	return r
+}
+
+func getKey(r *apiClient.Request) string {
+	return r.NetRequest.URL.RawQuery
 }
